0c486c1ca0fb9bebd9f629e1099a206fc5e6bc08
Ledary <andrejkulishov@gmail.com>
2021-05-22 01:15:10 +0100

    Structure rebuild, as well old projects removed

--- newyear/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-import datetime
-
-# Create your views here.
-date = datetime.datetime.now()
-def index(request):
-    return render(request, "newyear/index.html", {
-        "date": date.month == 1 and date.day == 1
-    })
