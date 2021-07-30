8a100033cb0c85cc60e924db73a74fd56012e32a
Ledary <andrejkulishov@gmail.com>
2021-07-30 18:01:59 +0100

    old server conf deleted

--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import os, sys
-sys.path.insert(0, '/var/www/u1074205/data/www/ledary.com/backend')
-sys.path.insert(1, '/var/www/u1074205/data/djangoenv/lib/python3.9/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
