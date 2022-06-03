41e4edae1dbf794d2337d4ed7767386620966bab
Ledaryy <andrew.ledary@gmail.com>
2022-06-04 00:08:11 +0100

    favicon bugfix

--- webhost/webhost/urls.py
+++ webhost/webhost/urls.py
@@ -1,22 +1,27 @@
-"""backend URL Configuration
+#
+# Author: Andrew Kulishov <support@andrewkulishov.com>
+# Copyright (C) 2022 Andrew Kulishov - All Rights Reserved
+#
+# Created on Fri Jun 03 2022
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+#
+# If there are any issues contact me on the email above.
+#
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("portfolio.urls")),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("static/img/favicon.ico")),
+    ),
 ]
